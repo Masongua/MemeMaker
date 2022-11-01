63475f3eb8a59c19f855a0ef5e18b5be0ac892b0
Mason Guaman <masonstalin@gmail.com>
2022-11-01 14:15:27 -0400

    🧽

---
+++ CaptionOption.swift
@@ -0,0 +1,21 @@
+//
+//  CaptionOption.swift
+//  MemeMaker
+//
+//  Created by 5e on 11/1/22.
+//
+
+import Foundation
+
+
+struct CaptionOption {
+    var emoji: String
+    var caption: String
+    
+    
+    init(emoji: String, caption: String) {
+        self.emoji = emoji
+        self.caption = caption
+        
+    }
+}
--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var topSegmentedControl: UISegmentedControl!
+    
+    
+    @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    
+    
+    @IBOutlet weak var topCaptionLabel: UILabel!
+    
+    @IBOutlet weak var bottomCaptionLabel: UILabel!
+    
+    
+    
+    
+    @IBAction func segmentedControlAct(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var topChoices: [String] = [CaptionOption(emoji: "🐹", caption: "Ham"), CaptionOption(emoji: "🫥", caption: "bruh")]
+    
+    var bottomChoices: [String] = [CaptionOption(emoji: "👦", caption: "boy"), CaptionOption(emoji: "🧽", caption: "sponge")]
+    
+    
 
 
 }
